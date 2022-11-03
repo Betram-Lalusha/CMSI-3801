0bf8051631ef9474d1500fc2ac6f2dcd2eb54018
Betram Lalusha <lalushabetm@gmail.com>
2022-11-03 00:08:11 -0700

    idiomatic Swift function

--- swift/exercises.swift
+++ swift/exercises.swift
@@ -56,3 +56,102 @@ func twice<T>(_ f: (T)->T, appliedTo x: T) -> T {
 func uppercasedFirst(of:[String], longerThan: Int) -> String? {
     return of.first(where: {$0.count > longerThan})?.uppercased() ?? nil
 }
+
+struct Quaternion {
+    var a: Double
+    var b: Double
+    var c: Double
+    var d: Double
+
+    init(a: Double, b: Double, c: Double, d: Double) {
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+    }
+
+    // func + (q2: Quaternion) -> Quaternion {
+    //     return Quaternion(
+    //         a: self.a + q2.a,
+    //         b: self.b + q2.b,
+    //         c: self.c + q2.c,
+    //         d: self.d + q2.d
+    //     )
+    // }
+
+    // func - (q2: Quaternion) -> Quaternion  {
+    //     return Quaternion(
+    //         a: self.a - q2.a,
+    //         b: self.b - q2.b,
+    //         c: self.c - q2.c,
+    //         d: self.d - q2.d
+    //     )
+    // }
+
+    // func * (q2: Quaternion) -> Quaternion {
+    //     let a0 = self.a * q2.a - self.b * q2.b - self.c * q2.c - self.d * q2.d   
+    //     let b0 = self.a * q2.b + self.b * q2.a + self.c * q2.d - self.d * q2.c   
+    //     let c0 =  self.a * q2.c - self.b * q2.d + self.c * q2.a + self.d * q2.b
+    //     let d0 = self.a * q2.d + self.b * q2.c - self.c * q2.b + self.d * q2.a
+    //     return Quaternion(a: a0, b: b0, c: c0, d: d0)
+    // }
+
+        var description: String {
+            print("\(self.a)\(self.b)i\(self.c)j\(self.d)k")
+            return "\(self.a)\(self.b)i\(self.c)j\(self.d)k"
+    }
+}
+
+protocol Animal {
+    var name: String { get }
+    var sound: String { get }
+    
+}
+
+extension Animal {
+    func speak() -> String {
+        return("\(name) says \(sound)")
+    }
+}
+
+struct Horse: Animal {
+    var name: String
+    var sound = "neigh"
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func speak() -> String {
+        return "\(name) says \(sound)"
+    }
+}
+
+struct Cow: Animal {
+    var name: String
+    var sound = "moooo"
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func speak() -> String {
+        return "\(name) says \(sound)"
+    }
+}
+
+struct Sheep: Animal {
+    var name: String
+    var sound = "baaaa"
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func speak() -> String {
+        return "\(name) says \(sound)"
+    }
+}
+
+
+
--- swift/main.swift
+++ swift/main.swift
@@ -3,22 +3,22 @@ import Foundation
 // First check ensures you defined your own struct NegativeAmountError.
 // If you implement your solution correctly, you will get a warning 
 // when you compile, that's ok!
-assert(change(200) is Result<(Int, Int, Int, Int), NegativeAmountError>)
-switch change(250) {
-    case .success(let coins): assert(coins == (10, 0, 0, 0))
-    case _: assert(false)
-}
-switch change(-50) {
-    case .success(_): assert(false)
-    case .failure(let error): assert(error is NegativeAmountError)
-}
-assert(try! change(0).get() == (0, 0, 0, 0))
-assert(try! change(97).get() == (3, 2, 0, 2))
-assert(try! change(8).get() == (0, 0, 1, 3))
-assert(try! change(144).get() == (5, 1, 1, 4))
-assert(try! change(97).get() == (3, 2, 0, 2))
-assert(try! change(100000000000).get() == (4000000000, 0, 0, 0))
-assert((try? change(-50).get()) == nil)
+// assert(change(200) is Result<(Int, Int, Int, Int), NegativeAmountError>)
+// switch change(250) {
+//     case .success(let coins): assert(coins == (10, 0, 0, 0))
+//     case _: assert(false)
+// }
+// switch change(-50) {
+//     case .success(_): assert(false)
+//     case .failure(let error): assert(error is NegativeAmountError)
+// }
+// assert(try! change(0).get() == (0, 0, 0, 0))
+// assert(try! change(97).get() == (3, 2, 0, 2))
+// assert(try! change(8).get() == (0, 0, 1, 3))
+// assert(try! change(144).get() == (5, 1, 1, 4))
+// assert(try! change(97).get() == (3, 2, 0, 2))
+// assert(try! change(100000000000).get() == (4000000000, 0, 0, 0))
+// assert((try? change(-50).get()) == nil)
 
 assert("".stretched == "")
 assert("  ".stretched == "")
@@ -44,17 +44,17 @@ scratch.removeAll()
 powers(of: -3, through: 300) { scratch.append($0) }
 assert(scratch == [1, -3, 9, -27, 81, -243])
 
-// let h: Animal = Horse(name: "CJ")
-// assert(h.speak() == "CJ says neigh")
-// let c: Animal = Cow(name: "Bessie")
-// assert(c.speak() == "Bessie says moooo")
-// assert(Sheep(name: "Little Lamb").speak() == "Little Lamb says baaaa")
+let h: Animal = Horse(name: "CJ")
+assert(h.speak() == "CJ says neigh")
+let c: Animal = Cow(name: "Bessie")
+assert(c.speak() == "Bessie says moooo")
+assert(Sheep(name: "Little Lamb").speak() == "Little Lamb says baaaa")
 
-// // Test that Animal really is a protocol with the default method
-// struct Rat: Animal {
-//     let name: String
-//     let sound = "squeak"
-// }
+// Test that Animal really is a protocol with the default method
+struct Rat: Animal {
+    let name: String
+    let sound = "squeak"
+}
 // assert(Rat(name:"Oreo").speak() == "Oreo says squeak")
 
 // assert(say("A").phrase == "A")
@@ -73,11 +73,11 @@ assert(uppercasedFirst(of: ["a", "bcdef"], longerThan: 5) == nil)
 assert(uppercasedFirst(of: ["a", "abcdef", "g"], longerThan: 5) == Optional.some("ABCDEF"))
 assert(uppercasedFirst(of: ["ab", "abcf"], longerThan: 1) == Optional.some("AB"))
 
-// let q = Quaternion(a: 3.5, b: 2.25, c: -100, d: -1.25)
-// assert(q.a == 3.5)
-// assert(q.b == 2.25)
-// assert(q.c == -100.0)
-// assert(q.d == -1.25)
+let q = Quaternion(a: 3.5, b: 2.25, c: -100, d: -1.25)
+assert(q.a == 3.5)
+assert(q.b == 2.25)
+assert(q.c == -100.0)
+assert(q.d == -1.25)
 // let q1 = Quaternion(a: 1, b: 3, c: 5, d: 2)
 // let q2 = Quaternion(a: -2, b: 2, c: 8, d: -1)
 // let q3 = Quaternion(a: -1, b: 5, c: 13, d: 1)
