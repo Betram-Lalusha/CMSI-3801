dcbdc60765f7d72b97aa129f680ce003f6f4ceb8
Betram Lalusha <lalushabetm@gmail.com>
2022-11-06 10:56:19 -0800

    including exercises.h

--- cpp/exercises.cpp
+++ cpp/exercises.cpp
@@ -1,10 +1,15 @@
-//copied from class notes
-vector<pair<string, int>> sorted_word_counts(list<string> words) {
+#include "exercises.h"
+
+// copied from class notes
+vector<pair<string, int>> sorted_word_counts(list<string> words)
+{
   map<string, int> counts;
-  for (string word : words) {
+  for (string word : words)
+  {
     counts[word]++;
   }
-  auto value_descending = [](auto x, auto y) { return y.second < x.second; };
+  auto value_descending = [](auto x, auto y)
+  { return y.second < x.second; };
   vector<pair<string, int>> pairs(counts.begin(), counts.end());
   sort(pairs.begin(), pairs.end(), value_descending);
   return pairs;
